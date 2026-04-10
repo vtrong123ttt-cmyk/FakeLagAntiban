5f1f5cb1388982dc80a2199c9cdc88f757dd9447
vtrong123ttt-cmyk <vtrong123ttt@gmail.com>
2026-04-10 17:35:13 +0700

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -1,6 +1,7 @@
 #include <substrate.h>
 #include <unistd.h>
 #include <sys/sysctl.h>
+#include <string.h>
 
 // --- FAKE LAG ---
 ssize_t (*old_send)(int sockfd, const void *buf, size_t len, int flags);
@@ -12,7 +13,9 @@ ssize_t new_send(int sockfd, const void *buf, size_t len, int flags) {
 // --- ANTIBAN ---
 int (*old_access)(const char *path, int mode);
 int new_access(const char *path, int mode) {
-    if (strstr(path, "Library/MobileSubstrate") || strstr(path, "Cydia")) return -1;
+    if (path && (strstr(path, "Library/MobileSubstrate") || strstr(path, "Cydia"))) {
+        return -1;
+    }
     return old_access(path, mode);
 }
 
